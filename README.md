3d5bc0407eb39968fddc990f848d3b5a9f8120fb
huseinzol05 <husein.zol05@gmail.com>
2025-02-06 04:28:51 +0800

    update some help in ring flex

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # context-parallelism
+
+Support Ring Attention https://arxiv.org/abs/2310.01889 and Tree Attention https://arxiv.org/abs/2408.04093 using Flex Attention.
--- context_parallelism/ring_flex.py
+++ context_parallelism/ring_flex.py
@@ -3,6 +3,86 @@ import torch.distributed as dist
 from torch.nn.attention.flex_attention import flex_attention, create_block_mask
 from .utils import merge_attention, RingComm, causal_mask
 
+"""
+For those who dont understand Blockwise Ring Attention,
+
+For simplicity sake, let say, we have 2 GPUs with seqlen of 4, partitioned to 2, 4 / 2 = 2, 2
+
+For GPU 0, should calculate q0k0v0 + q0k1v1
+For GPU 1, should calculate q1k0v0 + q1k1v1
+(+) denoted as blockwise attention.
+
+so the attention is like,
+
+      k0  | k1
+q0    o x | x x
+      o o | x x
+      ---------
+q1    o o | o x
+      o o | o o
+
+For GPU 0
+
+cp_rank = 0
+cp_world_size = 2
+send = (cp_rank + 1) % cp_world_size = 1
+receive = (cp_rank - 1) % cp_world_size = 1
+k = k0
+v = v0
+
+step == 0
+    if step + 1 != comm.world_size: True
+        send k, v to send
+        receive v1, v1 from receive
+
+    if not is_causal or step <= comm.rank: True
+        calculate attention with is_causal and step == 0, so this causal
+
+    if step + 1 != comm.world_size: True
+    k = k1
+    v = v1
+
+step == 1
+
+    if step + 1 != comm.world_size: False
+    if not is_causal or step <= comm.rank: False
+    if step + 1 != comm.world_size: False
+
+For GPU 1
+
+cp_rank = 1
+cp_world_size = 2
+send = (cp_rank + 1) % cp_world_size = 0
+receive = (cp_rank - 1) % cp_world_size = 0
+k = k1
+v = v1
+
+step == 0
+    if step + 1 != comm.world_size: True
+
+        send k, v to send
+        receive k0, v0 from receive
+
+    if not is_causal or step <= comm.rank: True
+
+        calculate attention with is_causal and step == 0, so this causal
+
+    if step + 1 != comm.world_size: True
+    k = k0
+    v = v0
+
+step == 1
+    if step + 1 != comm.world_size: False
+
+    if not is_causal or step <= comm.rank: True
+
+        calculate attention with is_causal and step == 1, full attention
+
+    if step + 1 != comm.world_size: False
+
+Everything hit as it is.
+"""
+
 def _forward(
     process_group,
     q: torch.Tensor,
